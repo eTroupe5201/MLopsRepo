55986d36492f9935a8d7bb91f9da54d63f3afb31
eTroupe5201 <etroupe@depaul.edu>
2024-04-18 17:49:54 -0500

    class code practice

---
+++ docs/source/index.md
@@ -0,0 +1,4 @@
+## Documentation
+
+Documentation for mlops_test_cookiecutter
+
---
+++ mlops_test_cookiecutter/__init__.py
@@ -0,0 +1,2 @@
+from mlops_test_cookiecutter.models.model import MyNeuralNet
+from mlops_test_cookiecutter.predict_model import predict
